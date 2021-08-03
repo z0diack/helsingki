852cb97fec98c699e6c84ff2b8f89dd64abd6ced
z0diack <jakevardy@gmail.com>
2021-08-03 21:06:11 +0100

    Revert "Removed website."
    This reverts commit 4320acc5cb2330818717815b3c2454cb2f1cdf20.

---
+++ js/gallery.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+var gallery = document.getElementById('gallery');
+
+fetch("/js/gallery.json")
+
+
+.then(function(res) {
+  res.json().then(function(json) {
+    json.forEach(function(el) {
+      var galleryItem = document.createElement('a');
+      galleryItem.setAttribute('class', 'gallery-item');
+      galleryItem.setAttribute('src', el.location);
+      galleryItem.setAttribute('target', '_blank');
+      var image = document.createElement('img');
+      image.setAttribute('src', el.location);
+      image.setAttribute('title', el.caption);  
+      image.setAttribute('alt', el.alt);
+
+      var caption = document.createElement('caption');
+      
+      caption.innerText = el.caption;
+
+      galleryItem.appendChild(image);
+      galleryItem.appendChild(caption);
+      
+
+      gallery.appendChild(galleryItem);
+    });
+  });
+});
+
+});
